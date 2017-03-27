7b7c5b0d49fb6ba932febf32159e598be5195ff3
Markos Chandras <mchandras@suse.de>
2017-03-27 07:19:49 +0100

    run_tests.sh: Use common test script to run the tox tests
    Update the run_tests.sh script to use the common script file
    from the openstack-ansible-tests repository in order to keep
    the testing logic into a central place and avoid duplication.
    This also updates the bindep.txt to match the one from the
    openstack-ansible-tests repository. Finally, CentOS7 support
    has been added to the Vagrantfile file.
    
    Change-Id: I2339bcf43384d50109ae8e958f70de9e63adbb76

--- run_tests.sh
+++ run_tests.sh
@@ -13,72 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o pipefail
-set -euov
+set -xeu
 
-FUNCTIONAL_TEST=${FUNCTIONAL_TEST:-true}
+source /etc/os-release || /usr/lib/os-release
 
-# Start fresh
-rm -rf .tox
+install_pkg_deps() {
+    pkg_deps="git"
 
-# Install python2 for Ubuntu 16.04,CentOS 7 and openSUSE Leap 42.1
-if which apt-get &>/dev/null && ! which zypper &>/dev/null; then
-     sudo apt-get update && sudo apt-get install -y python-dev
-elif which yum &>/dev/null; then
-     sudo yum install -y python-devel
-elif which zypper &>/dev/null; then
-    # Need to pull libffi and python-pyOpenSSL early
-    # because we install ndg-httpsclient from pip
-    sudo zypper -n in python-devel libffi-devel python-pyOpenSSL
-fi
+    case ${ID,,} in
+        *suse*) pkg_mgr_cmd="zypper -n in" ;;
+        centos|rhel) pkg_mgr_cmd="yum install -y" ;;
+        fedora) pkg_mgr_cmd="dnf -y install" ;;
+        ubuntu|debian) pkg_mgr_cmd="apt-get install -y" ;;
+        *) echo "unsupported distribution: ${ID,,}"; exit 1 ;;
+    esac
 
-# Install pip
-if ! which pip &>/dev/null; then
-    curl --silent --show-error --retry 5 \
-        https://bootstrap.pypa.io/get-pip.py | sudo python2.7
-fi
+    eval sudo $pkg_mgr_cmd $pkg_deps
+}
 
-# Install bindep and tox
-sudo pip install bindep tox
-
-# CentOS 7 requires two additional packages:
-#   redhat-lsb-core - for bindep profile support
-#   epel-release    - required to install python-ndg_httpsclient/python2-pyasn1
-if which yum &>/dev/null; then
-    sudo yum -y install redhat-lsb-core epel-release
-# openSUSE 42.1 does not have python-ndg-httpsclient
-elif which zypper &>/dev/null; then
-    pip install ndg-httpsclient
-fi
-
-# Get a list of packages to install with bindep. If packages need to be
-# installed, bindep exits with an exit code of 1.
-BINDEP_PKGS=$(bindep -b -f bindep.txt test || true)
-echo "Packages to install: ${BINDEP_PKGS}"
-
-# Install OS packages using bindep
-if [[ ${#BINDEP_PKGS} > 0 ]]; then
-    if which apt-get &>/dev/null && ! which zypper &>/dev/null ; then
-        sudo apt-get update
-        DEBIAN_FRONTEND=noninteractive \
-            sudo apt-get -q --option "Dpkg::Options::=--force-confold" \
-            --assume-yes install `bindep -b -f bindep.txt test`
-    elif which yum &>/dev/null; then
-        sudo yum install -y $BINDEP_PKGS
-    elif which zypper &>/dev/null; then
-        sudo zypper -n in $BINDEP_PKGS
+git_clone_repo() {
+    if [[ ! -d tests/common ]]; then
+        git clone https://git.openstack.org/openstack/openstack-ansible-tests tests/common
     fi
-fi
+}
 
-# run through each tox env and execute the test
-for tox_env in $(awk -F= '/envlist/ {print $2}' tox.ini | sed 's/,/ /g'); do
-    if [ "${tox_env}" != "ansible-functional" ]; then
-        tox -e ${tox_env}
-    elif [ "${tox_env}" == "ansible-functional" ]; then
-        if ${FUNCTIONAL_TEST}; then
-            tox -e ${tox_env}
-        fi
-    fi
-done
+install_pkg_deps
+
+git_clone_repo
+
+# start executing the main test script
+source tests/common/run_tests.sh
 
 # vim: set ts=4 sw=4 expandtab:
